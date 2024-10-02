91c434a9e6ef7e5ed991d4edadc151bc98e4ffe5
Ash Coles <ashley@acoles.co.uk>
2024-10-02 09:24:56 +0100

    Fixing applicant entity tests

---
+++ src/Hydrators/UserHydrator.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Portal\Hydrators;
+
+class UserHydrator
+{
+
+}
--- tests/Entities/ApplicantEntityTest.php
+++ tests/Entities/ApplicantEntityTest.php
@@ -3,19 +3,22 @@
 namespace Tests\Entities;
 
 use Portal\Entities\ApplicantEntity;
+use Portal\ValueObjects\EmailAddress;
 use Tests\TestCase;
 
 class ApplicantEntityTest extends TestCase
 {
     public function testConstruct(): void
     {
-        $case = new ApplicantEntity(1, 'test', 'test@test.com', '2024-01-01');
+        $email = $this->createMock(EmailAddress::class);
+        $case = new ApplicantEntity(1, 'test', $email, '2024-01-01');
         $this->assertInstanceOf(ApplicantEntity::class, $case);
     }
 
     public function testGetFormattedApplicationDate(): void
     {
-        $entity = new ApplicantEntity(1, 'test', 'test@test.com', '2024-01-01');
+        $email = $this->createMock(EmailAddress::class);
+        $entity = new ApplicantEntity(1, 'test', $email, '2024-01-01');
         $expected = '1st January, 2024';
         $actual = $entity->getFormattedApplicationDate();
         $this->assertEquals($expected, $actual);
