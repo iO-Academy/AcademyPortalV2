5c3de7b76c894b3c60c6e6c757ef12eb594c14c9
Ash Coles <ashley@acoles.co.uk>
2024-11-15 10:18:40 +0000

    Making base controller abstract

--- src/Controllers/Controller.php
+++ src/Controllers/Controller.php
@@ -3,12 +3,16 @@
 namespace Portal\Controllers;
 
 use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
 
 /**
  * Controllers can extend this class to access some helpful redirection methods.
  */
-class Controller
+abstract class Controller
 {
+
+    abstract public function __invoke(Request $request, Response $response): Response;
+
     protected function redirect(Response $response, string $url): Response
     {
         return $response->withHeader('Location', $url)->withStatus(301);
