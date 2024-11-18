46978dbe0e3acbb718687abe04e5baf163998882
Liam O'Connell <liamslost@googlemail.com>
2024-11-18 15:29:49 +0000

    change max string length to 255 in string validator

--- src/Validators/StringValidator.php
+++ src/Validators/StringValidator.php
@@ -11,16 +11,16 @@ class StringValidator
      */
     public static function validateLength(
         string $string,
-        int $maxLength,
-        int $minLength = 0,
+        int    $maxLength = 255,
+        int    $minLength = 0,
         string $fieldName = 'unknown'
-    ): bool {
+    ): bool
+    {
         $len = strlen($string);
 
         if ($len < $minLength || $len > $maxLength) {
             throw new Exception("$fieldName: Length invalid");
         }
-
         return true;
     }
 }
