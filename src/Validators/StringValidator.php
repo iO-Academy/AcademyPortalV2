3eb837419794496c38fc6d726432dc6f141b53a7
jordan_brewer <jordanbrewer1994@gmail.com>
2024-11-19 11:39:13 +0000

    edited validate postcode function

--- src/Validators/StringValidator.php
+++ src/Validators/StringValidator.php
@@ -26,6 +26,7 @@ class StringValidator
 
     public static function  validatePostcode ($string)
     {
+        $string = trim($string);
         return (bool) preg_match('/^([ A-Za-z]{1,2}[0-9]{1,2}[ A-Za-z]?)\s?([0-9][A-Za-z]{2}?)$/', $string);
     }
 }
