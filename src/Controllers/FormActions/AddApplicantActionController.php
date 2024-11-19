56b16229ca453eb99edb0b719073cb1836e653c3
vincedefreitas <97405426+vincedefreitas@users.noreply.github.com>
2024-11-19 14:57:37 +0000

    Added validation to AddApplicantActionController

--- src/Controllers/FormActions/AddApplicantActionController.php
+++ src/Controllers/FormActions/AddApplicantActionController.php
@@ -7,6 +7,7 @@ use Portal\Controllers\Controller;
 use Portal\Models\ApplicantsModel;
 use Portal\Services\AuthService;
 use Portal\Validators\ApplicantValidator;
+use Portal\Validators\ApplicationValidator;
 use Portal\ValueObjects\EmailAddress;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
@@ -32,6 +33,30 @@ class AddApplicantActionController extends Controller
 
         try {
             ApplicantValidator::validate($newApplicant);
+            ApplicationValidator::validate($newApplicant);
+
+            $circumstances = $this->model->getAllCircumstances();
+            if (!is_numeric($newApplicant['circumstance_id']) && !in_array($newApplicant['circumstance_id'], $circumstances)) {
+                throw new Exception("Circumstance ID doesn't exist");
+            }
+
+            $fundingOptions = $this->model->getAllFundingOptions();
+            if (!is_numeric($newApplicant['funding_id']) && !in_array($newApplicant['funding_id'], $fundingOptions)) {
+                throw new Exception("Funding ID doesn't exist");
+            }
+
+            $cohorts = $this->model->getAllCohorts();
+            if (!is_numeric($newApplicant['cohorts_id']) && !in_array($newApplicant['cohort_id'], $cohorts)) {
+                throw new Exception("Cohort ID doesn't exist");
+            }
+
+            $heardAbouts = $this->model->getAllHearAboutUs();
+            if (!in_array($newApplicant['heard_about_id'], $heardAbouts)) {
+                throw new Exception("Heard About ID doesn't exist");
+            }
+
+
+
         } catch (Exception $e) {
             return $this->redirectWithError($response, '/admin/applicant/add', $e->getMessage());
         }
