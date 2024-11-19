f00fbf1ad1937a1f5e93d9ff459d1af243bc4469
DBaldwin8 <darren_baldwin@live.com>
2024-11-19 15:58:44 +0000

    removed unused functions

--- src/Validators/StringValidator.php
+++ src/Validators/StringValidator.php
@@ -25,17 +25,4 @@ class StringValidator
         return true;
     }
 
-    public static function  validatePostcode ($string)
-    {
-        return (bool) preg_match('/^([ A-Za-z]{1,2}[0-9]{1,2}[ A-Za-z]?)\s?([0-9][A-Za-z]{2}?)$/', $string);
-    }
-
-    public static function validateDate($date, $format = 'Y-m-d'): bool
-    {
-        $d = DateTime::createFromFormat($format, $date);
-        return $d && $d->format($format) === $date;
-
-    }
-
-
 }
