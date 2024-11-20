269dc8ea176582f38cfaaba676db77027711cac9
DBaldwin8 <darren_baldwin@live.com>
2024-11-20 10:54:17 +0000

    AddApplicationActionController sniff warnings

--- src/Controllers/FormActions/AddApplicantActionController.php
+++ src/Controllers/FormActions/AddApplicantActionController.php
@@ -34,13 +34,25 @@ class AddApplicantActionController extends Controller
         try {
             ApplicantValidator::validate($newApplicant);
             ApplicationValidator::validate($newApplicant);
-            ApplicationValidator::checkExists($newApplicant['circumstance_id'], $this->model->getAllCircumstanceOptions()[$newApplicant['circumstance_id'] - 1], "Circumstance ID");
+            ApplicationValidator::checkExists(
+                $newApplicant['circumstance_id'],
+                $this->model->getAllCircumstanceOptions()[$newApplicant['circumstance_id'] - 1],
+                "Circumstance ID");
             ApplicationValidator::checkNumeric($newApplicant['circumstance_id'], "Circumstance ID");
-            ApplicationValidator::checkExists($newApplicant['funding_id'], $this->model->getAllFundingOptions()[$newApplicant['funding_id'] - 1], "Funding ID");
+            ApplicationValidator::checkExists(
+                $newApplicant['funding_id'],
+                $this->model->getAllFundingOptions()[$newApplicant['funding_id'] - 1],
+                "Funding ID");
             ApplicationValidator::checkNumeric($newApplicant['funding_id'], "Funding ID");
-            ApplicationValidator::checkExists($newApplicant['cohort_id'], $this->model->getAllCohorts()[$newApplicant['cohort_id'] - 1], "Cohort ID");
+            ApplicationValidator::checkExists(
+                $newApplicant['cohort_id'],
+                $this->model->getAllCohorts()[$newApplicant['cohort_id'] - 1],
+                "Cohort ID");
             ApplicationValidator::checkNumeric($newApplicant['cohort_id'], "Cohort ID");
-            ApplicationValidator::checkExists($newApplicant['heard_about_id'], $this->model->getAllHearAboutUsOptions()[$newApplicant['heard_about_id'] - 1], "Heard About ID");
+            ApplicationValidator::checkExists(
+                $newApplicant['heard_about_id'],
+                $this->model->getAllHearAboutUsOptions()[$newApplicant['heard_about_id'] - 1],
+                "Heard About ID");
             ApplicationValidator::checkNumeric($newApplicant['heard_about_id'], "Heard About ID");
         } catch (Exception $e) {
             return $this->redirectWithError($response, '/admin/applicant/add', $e->getMessage());
