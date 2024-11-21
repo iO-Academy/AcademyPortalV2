e329c517aaae9c153f33a3a93cf4c352c8fb6aad
DBaldwin8 <darren_baldwin@live.com>
2024-11-21 16:18:47 +0000

    added validation as it is used in AddApplicantActionController to EditApplicantActionController

--- src/Controllers/FormActions/EditApplicantActionController.php
+++ src/Controllers/FormActions/EditApplicantActionController.php
@@ -5,6 +5,7 @@ namespace Portal\Controllers\FormActions;
 use Exception;
 use Portal\Controllers\Controller;
 use Portal\Models\ApplicantsModel;
+use Portal\Models\CohortsModel;
 use Portal\Services\AuthService;
 use Portal\Validators\ApplicantValidator;
 use Portal\Validators\ApplicationValidator;
@@ -13,13 +14,15 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 
 class EditApplicantActionController extends Controller
 {
-    private $model;
+    private $applicantsModel;
+    private $cohortsModel;
     private $authService;
 
-    public function __construct(ApplicantsModel $model, AuthService $authService)
+    public function __construct(ApplicantsModel $applicantsModel, AuthService $authService, CohortsModel $cohortsModel)
     {
-        $this->model = $model;
+        $this->applicantsModel = $applicantsModel;
         $this->authService = $authService;
+        $this->cohortsModel= $cohortsModel;
     }
 
     public function __invoke(Request $request, Response $response, $args): Response
@@ -28,17 +31,32 @@ class EditApplicantActionController extends Controller
             return $this->redirect($response, '/');
         }
 
-        $details = $request->getParsedBody();
-        $details['id'] = $args['id'];
+        $input = $request->getParsedBody();
+        $input['id'] = $args['id'];
+        $hasApplication = false;
 
-//        try {
-//            ApplicantValidator::validate($details);
-//            ApplicationValidator::validate($details);
-//        } catch (Exception $e) {
-//            return $this->redirectWithError($response, '/admin/applicant/edit/'.$details['id'], $e->getMessage());
-//        }
+        try {
+            $changedApplicant = ApplicantValidator::validate($input);
+        } catch (Exception $e) {
+            return $this->redirectWithError($response, '/admin/applicant/add', $e->getMessage());
+        }
 
-        $this->model->editApplicant($details);
+        try {
+            $changedApplicant = ApplicationValidator::validate($input, $this->applicantsModel, $this->cohortsModel);
+            $hasApplication = true;
+        } catch (Exception $e) {
+            $hasApplication = false;
+        }
+
+        try {
+            $applicantId = $this->applicantsModel->editApplicant($changedApplicant);
+            if ($hasApplication && $applicantId) {
+                $this->applicantsModel->editApplication($changedApplicant, $applicantId);
+            }
+        } catch (Exception $e) {
+            return $this->redirectWithError($response, '/admin/applicant/add', $e->getMessage());
+        }
+
 
         return $response->withHeader('Location', '/admin/applicants')->withStatus(301);
     }
