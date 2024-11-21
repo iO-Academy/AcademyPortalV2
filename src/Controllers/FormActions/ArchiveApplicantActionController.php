6e89e3f21f3e4330fe0f7c539a05b8063623739d
jordan_brewer <jordanbrewer1994@gmail.com>
2024-11-21 14:05:51 +0000

    Authentication service added to archive applicant action controller

--- src/Controllers/FormActions/ArchiveApplicantActionController.php
+++ src/Controllers/FormActions/ArchiveApplicantActionController.php
@@ -2,20 +2,32 @@
 
 namespace Portal\Controllers\FormActions;
 
+use Portal\Controllers\Controller;
 use Portal\Models\ApplicantsModel;
+use Portal\Services\AuthService;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
 
-class ArchiveApplicantActionController
+
+
+class ArchiveApplicantActionController extends Controller
 {
 
     private $model;
+        private $authService;
 
-    public function __construct(ApplicantsModel $model)
+    public function __construct(ApplicantsModel $model, AuthService $authService)
     {
         $this->model = $model;
+        $this->authService = $authService;
     }
 
-    public function __invoke($request, $response, $args)
+    public function __invoke(Request $request, Response $response, $args): Response
     {
+        if (!$this->authService->isLoggedIn()) {
+            return $this->redirect($response, '/');
+        }
+
         $applicantId = $args['id'];
         $this->model->archiveApplicant($applicantId);
         return $response->withHeader('location', '/admin/applicants')->withStatus(301);
